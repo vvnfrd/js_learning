e4fa85200bc35f0fb53d2edf2b7e664ffb75409b
vvnfrd <zetmarley@outlook.com>
2025-05-28 11:11:35 +0700

    pkg json & prjct config lesson

--- app.js
+++ app.js
@@ -20,7 +20,39 @@
 // zetmarley.print();
 // zetmarley.sayHi();
 
-const messages = require('./messages');
+// const messages = require('./messages');
 
-messages.getMorningMessage();
-messages.getEveningMessage();
+// messages.getMorningMessage();
+// messages.getEveningMessage();
+
+// const nodePath = process.argv[0];
+// const appPath = process.argv[1];
+// const username = process.argv[2];
+// const userage = process.argv[3];
+
+// console.log("nodePath:", nodePath);
+// console.log("appPath:", appPath);
+// console.log();
+// console.log("name:", username);
+// console.log("age:", userage);
+
+// const lodash = require('lodash')
+
+// const people = ["Tom", "Sam", "Bob"];
+// const employees = ["Tom", "Alice", "Sam"];
+
+// const result1 = lodash.union(people, employees);
+// console.log(result1);
+
+// const result2 = lodash.intersection(people, employees);
+// console.log(lodash);
+
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", function(_, response) {
+//     response.end("Hello OPA OPA OPA");
+// });
+
+// app.listen(3000, function(){ console.log("http://localhost:3000/")})
