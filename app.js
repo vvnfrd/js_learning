163aa62ee94086df2988b38754d50c32a2257984
vvnfrd <zetmarley@outlook.com>
2025-05-29 14:39:35 +0700

    sequilize in mysql

--- app.js
+++ app.js
@@ -1,78 +1,34 @@
-const mysql = require("mysql2");
-const express = require("express");
- 
-const app = express();
-const urlencodedParser = express.urlencoded({extended: false});
- 
-const pool = mysql.createPool({
-  connectionLimit: 5,
-  host: "localhost",
-  user: "root",
-  database: "usersbd",
-  password: "13799731"
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("usersbd", "root", "13799731", {
+  dialect: "mysql",
+  host: "localhost"
 });
- 
-app.set("view engine", "hbs");
- 
-// получение списка пользователей
-app.get("/", function(req, res){
-    pool.query("SELECT * FROM users", function(err, data) {
-      if(err) return console.log(err);
-      res.render("index.hbs", {
-          users: data
-      });
-    });
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
 });
-// возвращаем форму для добавления данных
-app.get("/create", function(req, res){
-    res.render("create.hbs");
-});
-// получаем отправленные данные и добавляем их в БД 
-app.post("/create", urlencodedParser, function (req, res) {
-         
-    if(!req.body) return res.sendStatus(400);
-    const name = req.body.name;
-    const age = req.body.age;
-    pool.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
-      if(err) return console.log(err);
-      res.redirect("/");
-    });
-});
- 
-// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
-app.get("/edit/:id", function(req, res){
-  const id = req.params.id;
-  pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
-    if(err) return console.log(err);
-     res.render("edit.hbs", {
-        user: data[0]
-    });
-  });
-});
-// получаем отредактированные данные и отправляем их в БД
-app.post("/edit", urlencodedParser, function (req, res) {
-         
-  if(!req.body) return res.sendStatus(400);
-  const name = req.body.name;
-  const age = req.body.age;
-  const id = req.body.id;
-   
-  pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
-    if(err) return console.log(err);
-    res.redirect("/");
-  });
-});
- 
-// получаем id удаляемого пользователя и удаляем его из бд
-app.post("/delete/:id", function(req, res){
-          
-  const id = req.params.id;
-  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
-    if(err) return console.log(err);
-    res.redirect("/");
-  });
-});
- 
-app.listen(3000, function(){
-  console.log("Сервер ожидает подключения...");
-});
+
+sequelize.query("SELECT * FROM users;",
+  function(err, result){
+    if(err) console.log(err);
+    console.log(result);
+  }
+)
+
+sequelize.sync({force: true}).then(result=>{
+  // console.log(result);
+})
+.catch(err=> console.log(err));
--- old_coding/old_code.js
+++ old_coding/old_code.js
@@ -678,3 +678,83 @@
 //     }
 //     console.log("Подключение закрыто")
 //  })
+
+//Mysql + Express
+// const mysql = require("mysql2");
+// const express = require("express");
+ 
+// const app = express();
+// const urlencodedParser = express.urlencoded({extended: false});
+ 
+// const pool = mysql.createPool({
+//   connectionLimit: 5,
+//   host: "localhost",
+//   user: "root",
+//   database: "usersbd",
+//   password: "13799731"
+// });
+ 
+// app.set("view engine", "hbs");
+ 
+// // получение списка пользователей
+// app.get("/", function(req, res){
+//     pool.query("SELECT * FROM users", function(err, data) {
+//       if(err) return console.log(err);
+//       res.render("index.hbs", {
+//           users: data
+//       });
+//     });
+// });
+// // возвращаем форму для добавления данных
+// app.get("/create", function(req, res){
+//     res.render("create.hbs");
+// });
+// // получаем отправленные данные и добавляем их в БД 
+// app.post("/create", urlencodedParser, function (req, res) {
+         
+//     if(!req.body) return res.sendStatus(400);
+//     const name = req.body.name;
+//     const age = req.body.age;
+//     pool.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
+//       if(err) return console.log(err);
+//       res.redirect("/");
+//     });
+// });
+ 
+// // получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+// app.get("/edit/:id", function(req, res){
+//   const id = req.params.id;
+//   pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
+//     if(err) return console.log(err);
+//      res.render("edit.hbs", {
+//         user: data[0]
+//     });
+//   });
+// });
+// // получаем отредактированные данные и отправляем их в БД
+// app.post("/edit", urlencodedParser, function (req, res) {
+         
+//   if(!req.body) return res.sendStatus(400);
+//   const name = req.body.name;
+//   const age = req.body.age;
+//   const id = req.body.id;
+   
+//   pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
+//     if(err) return console.log(err);
+//     res.redirect("/");
+//   });
+// });
+ 
+// // получаем id удаляемого пользователя и удаляем его из бд
+// app.post("/delete/:id", function(req, res){
+          
+//   const id = req.params.id;
+//   pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+//     if(err) return console.log(err);
+//     res.redirect("/");
+//   });
+// });
+ 
+// app.listen(3000, function(){
+//   console.log("Сервер ожидает подключения...");
+// });
