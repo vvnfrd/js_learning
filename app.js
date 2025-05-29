dda3a9db00761c011285c686f4829a6c9c3ed260
vvnfrd <zetmarley@outlook.com>
2025-05-29 14:11:55 +0700

    Pools in Mysql

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
 const mysql = require("mysql2");
-  
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "usersbd",
-  password: "13799731"
-}).promise();
-connection.connect(function(err){
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    database: "usersbd",
+    password: "13799731"
+}).promise()
+
+// const sql = "INSERT INTO users (name, age) VALUES(?, ?)";
+// const data = ["Bill", 25];
+// pool.query(sql, data, function(err, results) {
+//   if(err) console.log(err);
+//   console.log(results);
+// });
+ 
+// получение объектов
+pool.query("SELECT * FROM users", function(err, results) {
+    if(err) console.log(err);
+    console.log(results);
+});
+
+pool.end(function(err) {
     if (err) {
-      return console.error("Ошибка: " + err.message);
+      console.log(err.message);
     }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
+    console.log("пул закрыт");
+  });
+  
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "usersbd",
+//   password: "13799731"
+// }).promise();
+// connection.connect(function(err){
+//     if (err) {
+//       return console.error("Ошибка: " + err.message);
+//     }
+//     else{
+//       console.log("Подключение к серверу MySQL успешно установлено");
+//     }
+//  });
 
- connection.query("SELECT * FROM users")
- .then(result =>{
-   console.log(result);
- })
- .catch(err =>{
-   console.log(err);
- });
+//  connection.query("SELECT * FROM users")
+//  .then(result =>{
+//    console.log(result);
+//  })
+//  .catch(err =>{
+//    console.log(err);
+//  });
 
- connection.end(function(err) {
-    if(err) {
-        return console.log("Ощибка" + err.message);
-    }
-    console.log("Подключение закрыто")
- })
+//  connection.end(function(err) {
+//     if(err) {
+//         return console.log("Ощибка" + err.message);
+//     }
+//     console.log("Подключение закрыто")
+//  })
