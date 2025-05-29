171e1d586e55b37747a94cd45f9cdcd4ff2363b9
vvnfrd <zetmarley@outlook.com>
2025-05-29 12:55:21 +0700

    Learning CRUD in MongoDB

--- app.js
+++ app.js
@@ -1,110 +1,20 @@
-const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const objectId = require("mongodb").ObjectId;
-      
-const app = express();
-app.use(express.static("public"));  // статические файлы будут в папке public
-app.use(express.json());        // подключаем автоматический парсинг json
-    
-const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
    
-(async () => {
-     try {
-        await mongoClient.connect();
-        app.locals.collection = mongoClient.db("usersdb").collection("users");
-        app.listen(3000);
-        console.log("Сервер ожидает подключения...");
-    }catch(err) {
-        return console.log(err);
-    } 
-})();
-   
-app.get("/api/users", async(req, res) => {
-           
-    const collection = req.app.locals.collection;
-    try{
-        const users = await collection.find({}).toArray();
-        res.send(users);
-    }
-    catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }  
+const userScheme = new Schema({
+    name: String,
+    age: Number
 });
-app.get("/api/users/:id", async(req, res) => {
-           
-    const collection = req.app.locals.collection;
-    try{
-        const id = new objectId(req.params.id);
-        const user = await collection.findOne({_id: id});
-        if(user) res.send(user);
-        else res.sendStatus(404);
-    }
-    catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-      
-app.post("/api/users", async(req, res)=> {
-          
-    if(!req.body) return res.sendStatus(400);
-          
-    const userName = req.body.name;
-    const userAge = req.body.age;
-    const user = {name: userName, age: userAge};
-          
-    const collection = req.app.locals.collection;
-       
-    try{
-        await collection.insertOne(user);
-        res.send(user);
-    }
-    catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-       
-app.delete("/api/users/:id", async(req, res)=>{
-           
-    const collection = req.app.locals.collection;
-    try{
-        const id = new objectId(req.params.id);
-        const user = await collection.findOneAndDelete({_id: id});
-        if(user) res.send(user);
-        else res.sendStatus(404);
-    }
-    catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-      
-app.put("/api/users", async(req, res)=>{
-           
-    if(!req.body) return res.sendStatus(400);
-    const userName = req.body.name;
-    const userAge = req.body.age;
-          
-    const collection = req.app.locals.collection;
-    try{
-        const id = new objectId(req.body.id);
-        const user = await collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName}},
-         {returnDocument: "after" });
-        if(user) res.send(user);
-        else res.sendStatus(404);
-    }
-    catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-    
-// прослушиваем прерывание работы программы (ctrl-c)
-process.on("SIGINT", async() => {
-       
-    await mongoClient.close();
-    console.log("Приложение завершило работу");
-    process.exit();
-});
+ 
+const User = mongoose.model("User", userScheme);
+ 
+async function main() {
+ 
+    await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
+     
+    const tom = new User({name: "Tom", age: 34});
+    // добавляем объект в БД
+    await tom.save();
+    console.log(tom);
+}
+main().catch(console.log).finally(async()=>await mongoose.disconnect());
