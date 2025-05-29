5e3cf33d31b6cc0f6d1b341f721e2bd68d2d5911
vvnfrd <zetmarley@outlook.com>
2025-05-29 14:22:45 +0700

    mysql + express

--- app.js
+++ app.js
@@ -1,59 +1,78 @@
 const mysql = require("mysql2");
-
-const pool = mysql.createPool({
-    connectionLimit: 5,
-    host: "localhost",
-    user: "root",
-    database: "usersbd",
-    password: "13799731"
-}).promise()
-
-// const sql = "INSERT INTO users (name, age) VALUES(?, ?)";
-// const data = ["Bill", 25];
-// pool.query(sql, data, function(err, results) {
-//   if(err) console.log(err);
-//   console.log(results);
-// });
+const express = require("express");
  
-// получение объектов
-pool.query("SELECT * FROM users", function(err, results) {
-    if(err) console.log(err);
-    console.log(results);
+const app = express();
+const urlencodedParser = express.urlencoded({extended: false});
+ 
+const pool = mysql.createPool({
+  connectionLimit: 5,
+  host: "localhost",
+  user: "root",
+  database: "usersbd",
+  password: "13799731"
 });
-
-pool.end(function(err) {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log("пул закрыт");
+ 
+app.set("view engine", "hbs");
+ 
+// получение списка пользователей
+app.get("/", function(req, res){
+    pool.query("SELECT * FROM users", function(err, data) {
+      if(err) return console.log(err);
+      res.render("index.hbs", {
+          users: data
+      });
+    });
+});
+// возвращаем форму для добавления данных
+app.get("/create", function(req, res){
+    res.render("create.hbs");
+});
+// получаем отправленные данные и добавляем их в БД 
+app.post("/create", urlencodedParser, function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    const name = req.body.name;
+    const age = req.body.age;
+    pool.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
+      if(err) return console.log(err);
+      res.redirect("/");
+    });
+});
+ 
+// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+app.get("/edit/:id", function(req, res){
+  const id = req.params.id;
+  pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+     res.render("edit.hbs", {
+        user: data[0]
+    });
   });
-  
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   database: "usersbd",
-//   password: "13799731"
-// }).promise();
-// connection.connect(function(err){
-//     if (err) {
-//       return console.error("Ошибка: " + err.message);
-//     }
-//     else{
-//       console.log("Подключение к серверу MySQL успешно установлено");
-//     }
-//  });
-
-//  connection.query("SELECT * FROM users")
-//  .then(result =>{
-//    console.log(result);
-//  })
-//  .catch(err =>{
-//    console.log(err);
-//  });
-
-//  connection.end(function(err) {
-//     if(err) {
-//         return console.log("Ощибка" + err.message);
-//     }
-//     console.log("Подключение закрыто")
-//  })
+});
+// получаем отредактированные данные и отправляем их в БД
+app.post("/edit", urlencodedParser, function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+  const name = req.body.name;
+  const age = req.body.age;
+  const id = req.body.id;
+   
+  pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+app.listen(3000, function(){
+  console.log("Сервер ожидает подключения...");
+});
