13092bc99f2ff909697116adc7a534ca6fe62f5e
vvnfrd <zetmarley@outlook.com>
2025-05-29 09:56:30 +0700

    api

--- app.js
+++ app.js
@@ -1,30 +1,89 @@
 const express = require("express");
-const expressHbs = require("express-handlebars");
-const hbs = require("hbs");
+     
 const app = express();
+app.use(express.json());
    
-// устанавливаем настройки для файлов layout
-app.engine("hbs", expressHbs.engine(
-    {
-        layoutsDir: __dirname + "/views/layouts", 
-        defaultLayout: "layout",
-        extname: "hbs"
+app.use(express.static("public"));
+ 
+// условная база данных
+const users = [];
+let id = 1;     // для установки идентификаторов
+ 
+// вспомогательная функция для поиска индекса пользователя по id
+function findUserIndexById(id){
+    for(let i=0; i < users.length; i++){
+        if(users[i].id==id) return i;
     }
-))
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-  
-app.use("/contact", function(_, response){
-       
-    response.render("contact", {
-        title: "Мои контакты",
-        email: "gavgav@mycorp.com",
-        phone: "+1234567890"
-    });
-}); 
-  
-app.use("/", function(_, response){
-       
-    response.render("home.hbs");
+    return -1;
+}
+app.get("/api/users", function(_, res){
+        
+    res.send(users);
 });
-app.listen(3000);
+// получение одного пользователя по id
+app.get("/api/users/:id", function(req, res){
+        
+    const id = req.params.id; // получаем id
+    // находим в массиве пользователя по id
+    const index = findUserIndexById(id);
+    // отправляем пользователя
+    if(index > -1){
+        res.send(users[index]);
+    }
+    else{
+        res.status(404).send("User not found");
+    }
+});
+// получение отправленных данных
+app.post("/api/users", function (req, res) {
+       
+    if(!req.body) return res.sendStatus(400);
+       
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = {name: userName, age: userAge};
+    // присваиваем идентификатор из переменной id и увеличиваем ее на единицу
+    user.id = id++;
+    // добавляем пользователя в массив
+    users.push(user);
+    res.send(user);
+});
+ // удаление пользователя по id
+app.delete("/api/users/:id", function(req, res){
+        
+    const id = req.params.id;
+    const index = findUserIndexById(id);
+    if(index > -1){
+        // удаляем пользователя из массива по индексу
+        const user = users.splice(index, 1)[0];
+        res.send(user);
+    }
+    else{
+        res.status(404).send("User not found");
+    }
+});
+// изменение пользователя
+app.put("/api/users", function(req, res){
+        
+    if(!req.body) return res.sendStatus(400);
+       
+    const id = req.body.id;
+    const userName = req.body.name;
+    const userAge = req.body.age;
+       
+    const index = findUserIndexById(id);
+    if(index > -1){
+        // изменяем данные у пользователя
+        const user = users[index];
+        user.age = userAge;
+        user.name = userName;
+        res.send(user);
+    }
+    else{
+        res.status(404).send("User not found");
+    }
+});
+    
+app.listen(3000, function(){
+    console.log("Сервер ожидает подключения...");
+});
--- old_coding/old_code.js
+++ old_coding/old_code.js
@@ -483,3 +483,33 @@
 // });
    
 // app.listen(3000);
+
+// Helper in hbs
+// const express = require("express");
+// const hbs = require("hbs");
+  
+// const app = express();
+  
+// hbs.registerHelper("getTime", function(){
+      
+//     const myDate = new Date();
+//     const hour = myDate.getHours();
+//     let minute = myDate.getMinutes();
+//     let second = myDate.getSeconds();
+//     if (minute < 10) {
+//         minute = "0" + minute;
+//     }
+//     if (second < 10) {
+//         second = "0" + second;
+//     }
+//     return `Текущее время: ${hour}:${minute}:${second}`;
+// });
+  
+// app.set("view engine", "hbs");
+  
+// app.get("/", function(_, response){
+      
+//     response.render("home.hbs");
+// });
+  
+// app.listen(3000);
