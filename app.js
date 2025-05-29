66cb5a451c4f98230a88114056fe17b379b7b316
vvnfrd <zetmarley@outlook.com>
2025-05-29 09:50:55 +0700

    Laoyuts in Hbs

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require("express");
+const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
 const app = express();
-  
+   
+// устанавливаем настройки для файлов layout
+app.engine("hbs", expressHbs.engine(
+    {
+        layoutsDir: __dirname + "/views/layouts", 
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
- 
-app.use("/contact", function(request, response){
-      
+  
+app.use("/contact", function(_, response){
+       
     response.render("contact", {
         title: "Мои контакты",
         email: "gavgav@mycorp.com",
         phone: "+1234567890"
     });
 }); 
- 
-app.use("/", function(request, response){
-      
+  
+app.use("/", function(_, response){
+       
     response.render("home.hbs");
 });
 app.listen(3000);
